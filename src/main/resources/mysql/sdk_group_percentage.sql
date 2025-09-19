170c265c87ef1fc07850d298d1a9f7d2c67f4681
luyao <lightyear5235@gmail.com>
2025-09-19 10:47:48 +0800

    update for surge

--- src/main/resources/mysql/sdk_group_percentage.sql
+++ src/main/resources/mysql/sdk_group_percentage.sql
@@ -11,7 +11,7 @@ CREATE TABLE `sdk_group_percentage` (
   `percentage` float DEFAULT '100' COMMENT '流量百分比',
   `exp_id` int(11) DEFAULT '0' COMMENT 'AB实验对应的id',
   `weight` int(11) DEFAULT '1' COMMENT '权重值',
-  `status` tinyint(4) NOT NULL DEFAULT '1'
+  `status` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
