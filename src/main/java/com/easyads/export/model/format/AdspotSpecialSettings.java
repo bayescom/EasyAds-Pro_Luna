8972f1d10bf6e2ef48d88febe10b35b5e76e68d2
luyao <lightyear5235@gmail.com>
2025-10-20 17:32:48 +0800

    update export for reward settings

--- src/main/java/com/easyads/export/model/format/AdspotSpecialSettings.java
+++
@@ -1,9 +0,0 @@
-package com.easyads.export.model.format;
-
-import com.easyads.management.adspot.model.RewardSetting;
-import lombok.Data;
-
-@Data
-public class AdspotSpecialSettings {
-    public RewardSetting reward;
-}
